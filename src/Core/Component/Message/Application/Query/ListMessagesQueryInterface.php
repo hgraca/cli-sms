7a9fd526e0c1a4ad22eac0372b0860b897404637
Herberto Graca <herberto.graca@gmail.com>
2018-10-18 15:30:16 +0200

    Create a query object to list messages

---
+++ src/Core/Component/Message/Application/Query/ListMessagesQueryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the CLI SMS application,
+ * which is created on top of the Explicit Architecture POC,
+ * which is created on top of the Symfony Demo application.
+ *
+ * This project is used in a workshop to explain Architecture patterns.
+ *
+ * Most of it authored by Herberto Graca.
+ */
+
+namespace Acme\App\Core\Component\Message\Application\Query;
+
+use Acme\App\Core\Port\Persistence\ResultCollectionInterface;
+
+interface ListMessagesQueryInterface
+{
+    public function find(array $orderBy = ['sentAt' => 'ASC'], int $limit = null): ResultCollectionInterface;
+}
